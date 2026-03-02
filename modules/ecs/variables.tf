f9e280d32a168596c048bc2fe76e4f4372be3dbd
felipesalesoliveira <fesales.oliveira@gmail.com>
2026-03-02 12:06:36 -0300

    feat: update

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,20 +1,93 @@
-variable "name" { type = string }
-variable "aws_region" { type = string }
-variable "vpc_id" { type = string }
-variable "public_subnet_ids" { type = list(string) }
-variable "private_subnet_ids" { type = list(string) }
-variable "container_image" { type = string }
-variable "container_name" { type = string default = "django-app" }
-variable "container_port" { type = number default = 8000 }
-variable "cpu" { type = number default = 512 }
-variable "memory" { type = number default = 1024 }
-variable "desired_count" { type = number default = 2 }
-variable "min_capacity" { type = number default = 2 }
-variable "max_capacity" { type = number default = 6 }
-variable "health_check_path" { type = string default = "/" }
-variable "environment_variables" { type = map(string) default = {} }
-variable "secret_arns" { type = map(string) default = {} }
-variable "assign_public_ip" { type = bool default = false }
-variable "alb_ingress_cidrs" { type = list(string) default = ["0.0.0.0/0"] }
-variable "certificate_arn" { type = string default = null }
-variable "tags" { type = map(string) default = {} }
+variable "name" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "container_image" {
+  type = string
+}
+
+variable "container_name" {
+  type    = string
+  default = "django-app"
+}
+
+variable "container_port" {
+  type    = number
+  default = 8000
+}
+
+variable "cpu" {
+  type    = number
+  default = 512
+}
+
+variable "memory" {
+  type    = number
+  default = 1024
+}
+
+variable "desired_count" {
+  type    = number
+  default = 2
+}
+
+variable "min_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "max_capacity" {
+  type    = number
+  default = 6
+}
+
+variable "health_check_path" {
+  type    = string
+  default = "/"
+}
+
+variable "environment_variables" {
+  type    = map(string)
+  default = {}
+}
+
+variable "secret_arns" {
+  type    = map(string)
+  default = {}
+}
+
+variable "assign_public_ip" {
+  type    = bool
+  default = false
+}
+
+variable "alb_ingress_cidrs" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "certificate_arn" {
+  type    = string
+  default = null
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
